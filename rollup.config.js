40f02fce674fdc6ba922f724f77dec98e86c322e
Clay Dunston <claydunston@gmail.com>
2022-06-13 18:52:04 -0500

    Meh

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,77 @@
 import typescript from "@rollup/plugin-typescript"
-// import commonjs from "@rollup/plugin-commonjs"
+import commonjs from "@rollup/plugin-commonjs"
+import babel from "@rollup/plugin-babel"
+import dts from "rollup-plugin-dts"
+import { terser } from "rollup-plugin-terser"
 
-/** @type {import('typescript').CompilerOptions} */
-const tsOptions = {
-    downlevelIteration: true,
-}
-
-/** @type {import('rollup').RollupOptions} */
-const config = {
-    input: "./src/lib/calculator/WeaponStatsCalculator.ts",
-    output: [
-        // {
-        //     // dir: "dist",
-        //     format: "cjs",
-        //     file: "./dist/cjs/elden-ring-calculator.js",
-        //     inlineDynamicImports: true,
-        // },
-        {
-            // dir: "dist",
-            format: "esm",
-            file: "./dist/rollup/elden-ring-calculator.mjs",
-            // inlineDynamicImports: true,
-        },
-    ],
-    // external: [
-
-    // ],
+const shared = {
+    input: "src/main.ts",
     plugins: [
         typescript({
             tsconfig: "tsconfig.prod.json",
             // inlineSources: true,
         }),
-        // commonjs({ extensions: [".js", ".ts"] }) // the ".ts" extension is required
+        // commonjs({ extensions: [".ts"] }), // the ".ts" extension is required
+        babel({ extensions: [".ts"], exclude: "node_modules/**" }),
+        // terser(),
+        // dts(),
     ],
 }
+
+/** @type {import('rollup').RollupOptions} */
+const config = [
+    // =====================================================================
+    // CJS
+    // =====================================================================
+    {
+        ...shared,
+        format: "cjs",
+        file: "./dist/index.cjs.min.js",
+        inlineDynamicImports: true,
+        // exports: "named",
+        plugins: [
+            ...shared.plugins,
+            commonjs({ extensions: [".ts"] }), // the ".ts" extension is require d
+        ],
+    },
+    // =====================================================================
+    // ESM
+    // =====================================================================
+    {
+        ...shared,
+        format: "es",
+        file: "./dist/index.es.js",
+        // inlineDynamicImports: true,
+    },
+    {
+        ...shared,
+        format: "esm",
+        file: "./dist/index.esm.js",
+        // inlineDynamicImports: true,
+    },
+    // =====================================================================
+    // UMD
+    // =====================================================================
+    {
+        ...shared,
+        file: "dist/index.umd.min.js",
+        format: "umd",
+        // name: "elden-ring-calculator",
+        indent: false,
+    },
+    // =====================================================================
+    // Types
+    // =====================================================================
+    {
+        ...shared,
+        file: "./dist/index.d.ts",
+        format: "es",
+        // name: "elden-ring-calculator",
+        plugins: [
+            ...shared.plugins,
+            dts(),
+        ],
+    },
+]
 
 export default config
